316854a55e18bcafb230da05a4e0407c937cc376
Jose Alberto Ruiz-Carrillo González <jruiz@bancocaminos.es>
2020-02-04 12:03:11 +0100

    ADDED: New project to repo

---
+++ UnitTestExample/UnitTestExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTestExample
+//
+//  Created by Jose Alberto Ruíz-Carrillo González on 04/02/2020.
+//  Copyright © 2020 JARCG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
